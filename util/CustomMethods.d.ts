7d20da20db58f870e31ddea5388ac9024fdeca07
Aneel Kumar <akumar@folio3pk.com>
2016-05-16 14:08:33 +0500

    category export functionality added

--- util/CustomMethods.d.ts
+++ util/CustomMethods.d.ts
@@ -7,6 +7,8 @@ declare class Utility {
 
     static logException(title:string, exception:any):void;
 
+    static logEmergency(title:string, description:string):void;
+    
     /**
      * Check if value exist or not
      * @param str
@@ -18,6 +20,14 @@ declare class MagentoWrapper {
 
 }
 
+declare class ExternalSystemWrapper {
+    public initialize(storeInfo:Store):void;
+    public getSessionIDFromServer(userName, apiKey);
+    public getCategories(rootCategoryId:any, depth:number):any;
+    public createCategory(internalCategory:Category, magentoParentCategoryId):any;
+    public updateCategory(internalCategory:Category, magentoParentCategoryId, magentoCategoryId):any;
+    public deleteCategory(id);
+}
 declare class ConnectorModels {
     static salesOrderModel():void;
 
@@ -25,4 +35,86 @@ declare class ConnectorModels {
 }
 declare class ConnectorConstants {
     static CurrentStore:any;
+    static initialize();
+    static loadItemConfigRecords();
+    static ExternalSystemConfig:Array<any>;
+    static CurrentWrapper:ExternalSystemWrapper;
 }
+declare class ConnectorCommon {
+    static createLogRec (recordId, requestData, recordType):any;
+    static initiateEmailNotificationConfig():any;
+}
+
+declare interface BaseRecord {
+    internalId?:string;
+}
+
+declare interface CategoryParentCategory extends BaseRecord {
+    name: string;
+}
+
+declare interface CategoryWebsite extends BaseRecord {
+    name: string;
+}
+
+declare interface Category extends BaseRecord {
+    childCategories?: Array<Category>;
+    parentCategory?: CategoryParentCategory;
+    itemId: string;
+    isInactive: boolean;
+    website?: any;
+    description?: any;
+    excludeFromSitemap?: boolean;
+    metaTagHtml?: string;
+    pageTitle?: string;
+    searchKeywords?: string;
+    urlComponent?: string;
+}
+
+declare class MC_SYNC_CONSTANTS {
+    static isValidLicense():boolean;
+}
+
+declare class F3WrapperFactory {
+    static getWrapper(type:any):any;
+}
+
+declare interface NetSuiteSOAPPassport {
+    email:string;
+    password:string;
+    account:string;
+}
+
+declare interface StoreNetSuite {
+    soapPassport?:NetSuiteSOAPPassport;
+}
+
+declare interface Store {
+    internalId:any;
+    systemId: any;
+    systemDisplayName: any;
+    systemDisplayNameValue: any;
+    userName: any;
+    password: any;
+    endpoint: any;
+    entitySyncInfo: any;
+    systemType: any;
+    permissions: any;
+    netsuite?: StoreNetSuite
+}
+
+type HttpMethod = "GET" | "POST" | "PUT" | "DELETE";
+
+interface HttpRequestData {
+    additionalUrl: string;
+    method: HttpMethod;
+    headers?: any;
+    postData?: any;
+    data?: any;
+}
+
+interface KeyValue<T> {
+    [s:string]: T;
+}
+
+type NetSuiteSOAPAction = "get";
--- util/f3_magento2_wrapper.ts
+++ util/f3_magento2_wrapper.ts
@@ -19,94 +19,311 @@
 // Declaration of Existing Custom Libraries methods
 /// <reference path="../util/CustomMethods.d.ts" />
 
-interface HttpRequestData {
-    additionalUrl: string;
-    method: string;
-    headers?: any;
-    postData?: any;
-    data?: any;
-}
-
+/**
+ * Wrapper class for Magento2 REST API
+ */
 class Magento2Wrapper {
-    private  ServerUrl = '';
-    private  UserName = '';
-    private  Password = '';
+    private serverUrl = '';
+    private username = '';
+    private password = '';
+    private token = '';
 
-    constructor() {
+    /**
+     * Initializes the wrapper for given store,
+     * setting up server url
+     *
+     * @param storeInfo
+     */
+    public initialize(storeInfo:Store):void {
+        if (!!storeInfo) {
+            this.serverUrl = storeInfo.endpoint;
+        } else if (!!ConnectorConstants && !!ConnectorConstants.CurrentStore) {
+            this.serverUrl = ConnectorConstants.CurrentStore.endpoint;
+        }
     }
 
-    private sendRequest(httpRequestData:HttpRequestData):void {
-        var finalUrl = this.ServerUrl + httpRequestData.additionalUrl;
+    /**
+     * Wrapper method for send nlapiRequestURL
+     *
+     * @param url
+     * @param postdata
+     * @param headers
+     * @param callback
+     * @param httpMethod
+     * @returns {nlobjResponse}
+     * @private
+     */
+    private _nlapiRequestURL (url : string, postdata? : any , headers? : any, callback? : any, httpMethod? : HttpMethod) {
+        url = url || null;
+        postdata = postdata || null;
+        headers = headers || {};
+        callback = callback || null;
+        httpMethod = httpMethod || null;
 
+        // this.setAuthHeaderIfNeeded(headers);
+
+        return nlapiRequestURL(url, postdata, headers, callback, httpMethod);
+    }
+
+    /**
+     * Method for sending request to Magento2 REST API
+     *
+     * @param httpRequestData
+     * @returns {any}
+     */
+    private sendRequest(httpRequestData:HttpRequestData):void {
+        var finalUrl = this.serverUrl + httpRequestData.additionalUrl;
         Utility.logDebug('Request final = ', finalUrl);
         var res = null;
-
         if (!httpRequestData.headers) {
             httpRequestData.headers = {
                 "Accept": "application/json",
                 "Content-Type": "application/json",
-                "Authorization": "Bearer " + this.Password
+                "Authorization": "Bearer " + this.token // this.Password
             };
         }
 
-        Utility.logDebug('httpRequestData = ', JSON.stringify(httpRequestData));
-
+        // Utility.logDebug('httpRequestData = ', JSON.stringify(httpRequestData));
         if (httpRequestData.method === 'GET') {
-            res = nlapiRequestURL(finalUrl, null, httpRequestData.headers);
-        } else {
-            var postDataString = typeof httpRequestData.postData === "object" ?
+            res = this._nlapiRequestURL(finalUrl, null, httpRequestData.headers);
+        }
+        else {
+            var postDataString =  httpRequestData.postData && typeof httpRequestData.postData === "object" ?
                 JSON.stringify(httpRequestData.postData) : httpRequestData.postData;
-
-            res = nlapiRequestURL(finalUrl, postDataString, httpRequestData.headers, httpRequestData.method);
+            res = this._nlapiRequestURL(finalUrl, postDataString, httpRequestData.headers, null, httpRequestData.method);
         }
 
         var body = res.getBody();
-        Utility.logDebug('Magento2 Response Body', body);
-        return eval('(' + body + ')');
+        // Utility.logDebug('Magento2 Response Body', body);
+
+        return JSON.parse(body);
     }
 
     /**
-     * Init method
-     * @param storeInfo
-     */
-    public initialize(storeInfo:any):void {
-        if (!!storeInfo) {
-            this.ServerUrl = storeInfo.endpoint;
-        } else if (!!ConnectorConstants && !!ConnectorConstants.CurrentStore) {
-            this.ServerUrl = ConnectorConstants.CurrentStore.endpoint;
-        }
-    }
-
-    /**
-     * Gets supported Date Format
+     * Sets up credential
+     *
+     * @param userName
+     * @param apiKey
      * @returns {string}
      */
-    public getDateFormat() {
-        return 'ISO';
-    }
-
     public getSessionIDFromServer(userName, apiKey) {
         var sessionID = 'DUMMY_SESSION_ID';
 
-        this.UserName = userName;
-        this.Password = apiKey;
+        this.username = userName;
+        this.password = apiKey;
+        this.token = apiKey;
 
         return sessionID;
     }
 
-    public getSalesOrders(filters:any):any {
-        return this.test("11");
+    /**
+     * Creates of updates the category and returns category id
+     *
+     * @param internalCategory
+     * @param magentoParentCategoryId
+     * @param magentoCategoryId
+     * @returns {string|any|number}
+     */
+    private createOrUpdateCategory(internalCategory:Category, magentoParentCategoryId, magentoCategoryId?):any {
+        var response;
+
+        var magentoCategory = Magento2WrapperUtility.getMagentoCategory(internalCategory, magentoParentCategoryId);
+        if (magentoCategory.id) magentoCategory.id = magentoCategoryId;
+        var httpRequestData:HttpRequestData = {
+            additionalUrl: 'categories',
+            method: 'POST',
+            postData: {"category":  magentoCategory}
+        };
+
+        response = this.sendRequest(httpRequestData);
+
+        return response.id;
     }
 
-    public test(id:string) {
-        var serverResponse;
-        var httpRequestData:HttpRequestData = {
-            additionalUrl: 'orders/' + id,
-            method: 'GET',
-            data: {}
-        };
-        serverResponse = this.sendRequest(httpRequestData);
+    /**
+     * Returns categories from category with
+     * given categoryId till given depth
+     *
+     * @param categoryId
+     * @param depth
+     */
+    public getCategories(categoryId, depth) {
+        return this.sendRequest({
+            additionalUrl: 'categories?rootCategoryId=' + categoryId + '&depth=' + depth,
+            method: 'GET'
+        });
+    }
 
-        return serverResponse;
+    /**
+     * Creates category for given params
+     *
+     * @param internalCategory
+     * @param magentoParentCategoryId
+     * @returns {any}
+     */
+    public createCategory(internalCategory:Category, magentoParentCategoryId):any {
+        return this.createOrUpdateCategory(internalCategory, magentoParentCategoryId);
+    }
+
+    /**
+     * Updates category for given params
+     *
+     * @param internalCategory
+     * @param magentoParentCategoryId
+     * @param magentoCategoryId
+     * @returns {any}
+     */
+    public updateCategory(internalCategory:Category, magentoParentCategoryId, magentoCategoryId):any {
+        return this.createOrUpdateCategory(internalCategory, magentoParentCategoryId, magentoCategoryId);
+    }
+
+    /**
+     * Deletes category for given id
+     *
+     * @param id
+     */
+    public deleteCategory(id) {
+        return this.sendRequest({
+            method: "DELETE",
+            additionalUrl: "categories/"+id
+        });
+    }
+
+    /**
+     * Deletes categories within (inclusive) given category id range
+     *
+     * @param startId
+     * @param endId
+     */
+    public deleteCategoriesInRange(startId, endId) {
+        for (var i = startId; i <= endId; ++i) {
+            this.deleteCategory(i);
+        }
+    }
+
+    /**
+     * Deletes categories with given category ids
+     * @param ids
+     */
+    public deleteCategoriesWithIds(ids) {
+        for (var i = ids.length - 1; i >= 0; --i) {
+            this.deleteCategory(ids[i]);
+        }
+    }
+}
+
+/**
+ * Interface for each of custom_attribute item
+ */
+interface Magento2CustomAttribute {
+    attribute_code:string;
+    value;
+}
+
+/**
+ * Key value interface to help create custom_attributes
+ */
+interface Magento2CustomAttributeValues {
+    description?;
+    meta_title?;
+    meta_keywords?;
+    meta_description?;
+    display_mode?;
+    is_anchor?;
+    path?;
+    children_count?;
+    custom_use_parent_settings?;
+    custom_apply_to_products?;
+    url_key?;
+    url_path?;
+}
+
+/**
+ * Interface for category in Magento2
+ */
+interface Magento2Category {
+    id?;
+    parent_id;
+    name: string;
+    is_active: boolean;
+    position?;
+    level?;
+    children?:string;
+    created_at?:string;
+    updated_at?:string;
+    path?:string;
+    available_sort_by?:Array<any>;
+    include_in_menu?: boolean;
+    extension_attributes?:any;
+    custom_attributes?: Array<Magento2CustomAttribute>;
+}
+
+/**
+ * Provides utility functions for Magento2Wrapper class
+ */
+class Magento2WrapperUtility {
+    /**
+     * Returns Magento2CustomAttribute object for given params
+     *
+     * @param attributeCode
+     * @param value
+     * @returns {{attribute_code: string, value: any}}
+     */
+    static getCustomAttribute(attributeCode:string,value):Magento2CustomAttribute {
+        return {
+            attribute_code: attributeCode,
+            value: value
+        }
+    }
+
+    /**
+     * Returns Array of Magento2CustomAttributes for given key value dictionary
+     *
+     * @param dictionary
+     * @returns {Array<Magento2CustomAttribute>}
+     */
+    static getCustomAttributes(dictionary:Magento2CustomAttributeValues):Array<Magento2CustomAttribute> {
+        var customAttributes:Array<Magento2CustomAttribute> = [];
+
+        for (var attrKey in dictionary) {
+            var attrValue = dictionary[attrKey];
+            if (attrValue || typeof (attrValue) != "undefined") {
+                customAttributes.push(this.getCustomAttribute(attrKey, attrValue));
+            }
+        }
+
+        return customAttributes;
+    }
+
+    /**
+     * Returns Magento2Category equivalent of internal Category
+     * for given parameters
+     *
+     * @param internalCategory
+     * @param magentoParentCategoryId
+     * @param magentoCategoryId
+     * @returns {Magento2Category}
+     */
+    static getMagentoCategory(internalCategory: Category, magentoParentCategoryId, magentoCategoryId?):Magento2Category {
+        var customAttributes = this.getCustomAttributes({
+            description: internalCategory.description,
+            meta_title: internalCategory.pageTitle,
+            meta_description: internalCategory.metaTagHtml,
+            meta_keywords: internalCategory.searchKeywords,
+            url_key: internalCategory.urlComponent // TODO: set a unique key if not existing
+        });
+
+        var magentoCategory:Magento2Category = {
+            include_in_menu: !internalCategory.excludeFromSitemap,
+            parent_id: magentoParentCategoryId || 1,
+            name: internalCategory.itemId,
+            // TODO: set the actual value for is_active when Magento2 API allows updating inactive category
+            is_active: true, //!internalCategory.isInactive,
+            custom_attributes: customAttributes
+        };
+
+        if (magentoCategoryId) magentoCategory.id = magentoCategoryId;
+
+        return magentoCategory;
     }
 }
